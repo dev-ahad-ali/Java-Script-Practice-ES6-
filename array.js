37bc574760b95d82e5a9a8f04579462ca320b906
Ahad Ali <ahadali.dev@gmail.com>
2024-01-07 12:15:20 +0600

    Array and Multidimensional array

---
+++ array.js
@@ -0,0 +1,9 @@
+var myArray=["A","B","C","D"];
+
+console.log(myArray[1]);
+
+var myArrayPro= new Array("a","b","c","d");
+
+for(let item of myArrayPro){
+    console.log(item);
+}
---
+++ multidimensional-array.js
@@ -0,0 +1,7 @@
+var bangladesh=["dhaka","rangpur","ctg"];
+var india=["mumbai",'chennai',"kolkata"];
+var china=["uhan","shanghai","taiwan"];
+
+var asia=[bangladesh,india,china];
+
+console.log(asia[1][2]);
